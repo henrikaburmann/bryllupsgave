534d8e41562c5f3d16ada6be13869e4f6eb93ca0
Henrik August Burmann <henrik.burmann@bouvet.no>
2026-08-14 06:46:38 +0200

    Vite config change back

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// // base is '/bryllupsgave/' for GitHub Pages project site; set to '/' when using a custom domain.
-// export default defineConfig({
-//   base: '/',
-//   plugins: [react()],
-// })
-
 // https://vite.dev/config/
 // base is '/bryllupsgave/' for GitHub Pages project site; set to '/' when using a custom domain.
 export default defineConfig({
-  base: '/bryllupsgave/',
+  base: '/',
   plugins: [react()],
 })
+
+// // https://vite.dev/config/
+// // base is '/bryllupsgave/' for GitHub Pages project site; set to '/' when using a custom domain.
+// export default defineConfig({
+//   base: '/bryllupsgave/',
+//   plugins: [react()],
+// })
